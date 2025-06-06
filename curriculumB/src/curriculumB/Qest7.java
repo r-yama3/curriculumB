dc6847bf87a0c8d6acc9364cd6f8e937427985f2
user <user@192.168.1.19>
2025-06-06 22:36:41 +0900

    カリキュラムB

--- curriculumB/src/curriculumB/Qest7.java
+++ curriculumB/src/curriculumB/Qest7.java
@@ -9,41 +9,52 @@ public class Qest7 {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("生徒の人数を入力してください（2以上）:");
 		int studentCount = scanner.nextInt(); //生徒の人数を入力
-		String[] subjects = {"英語","数学","理科","社会"}; //教科リスト
-		int subjectCount = subjects.length;
-		
+		String[] subjects = { "英語", "数学", "理科", "社会" }; //教科リスト
+		int subjectCount = subjects.length; //教科数
+
 		int[][] scores = new int[studentCount][subjectCount]; //人数・得点を保存
-		for (int i = 0; i < studentCount; i++) {
-			System.out.println();
-			for (int j =0 ; j < subjectCount; j++) {
-				System.out.print((i+1)+"人目の『"+subjects[j]+"』の点数を入力してください:");
-				scores[i][j] = scanner.nextInt();
+		for (int i = 0; i < studentCount; i++) { //〇人目
+			System.out.println(); //改行
+			for (int j = 0; j < subjectCount; j++) { //『教科』
+				System.out.print((i + 1) + "人目の『" + subjects[j] + "』の点数を入力してください:");
+				//〇人目の点数を入力
+				scores[i][j] = scanner.nextInt(); //各教科の点数を入力
 			}
 		}
-		for (int i = 0; i < studentCount; i++) {
+		scanner.close(); //入力を閉じる
+
+		for (int i = 0; i < studentCount; i++) { //〇人目
 			int total = 0;
-			System.out.println();
-			for (int j =0 ; j < subjectCount; j++) {
-			 total += scores[i][j];
+			System.out.println(); //改行
+			for (int j = 0; j < subjectCount; j++) { //『教科』
+				total += scores[i][j]; //〇人目の点数合計
 			}
-			double average = (double) total/ subjectCount;
-			System.out.printf((i+1)+"人目の平均点は%.2fです。\n",average);
+			double average = (double) total / subjectCount; //〇人目の平均点
+			System.out.printf((i + 1) + "人目の平均点は%.2fです。", average);
 		}
+
+		System.out.println(); //改行
+		for (int j = 0; j < subjectCount; j++) { //『教科』
+			int subjectTotal = 0;
+			System.out.println(); //改行
+			for (int i = 0; i < studentCount; i++) { //〇人目
+				subjectTotal += scores[i][j]; //各教科の点数の合計
+			}
+			double average = (double) subjectTotal / studentCount; //各教科の平均点
+			System.out.printf((subjects[j]) + "の平均点は%.2fです。", average);
+		}
+
+		System.out.println(); //改行
+		int totalScore = 0;
+		int count = 0;
+		for (int i = 0; i < studentCount; i++) { //〇人目
+			for (int j = 0; j < subjectCount; j++) { //『教科』
+				totalScore += scores[i][j]; //全ての点数の合計
+				count++; //要素数をカウント
+			}
+		}
+		double average = (double) totalScore / count; //全体の平均点
+		System.out.printf("全体の平均点は%.2fです。", average);
 	}
 }
-//N人の生徒の成績を入力できるようにしてください									
-//各生徒の科目平均点、全体の科目平均点、そして各教科の平均点を出力してください									
-//このプログラムの実行は必ず1回以上行われるようにしてください
-//生徒の人数を入力してください（2以上）: 
-//1人目の『英語』の点数を入力してください :			
-//1人目の『数学』の点数を入力してください :			
-//1人目の『理科』の点数を入力してください :			
-//1人目の『社会』の点数を入力してください :		
-
-//1人目の平均点は10.00点です。
-
-//英語の平均点は10.00点です。		
-//数学の平均点は10.00点です。		
-//理科の平均点は10.00点です。		
-//社会の平均点は10.00点です。		
-//全体の平均点は10.00点です。		
+		
